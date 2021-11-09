b571f1295f0313cdad58fa0b1b0e25907cbeb8c6
Sam Mahdi <65124322+sam-mahdi@users.noreply.github.com>
2021-11-09 14:03:02 -0500

    Update check_file_format.py

--- V2/APS/check_file_format.py
+++ V2/APS/check_file_format.py
@@ -18,7 +18,7 @@ def extract_experimental(data_file,data_directory):
 
     with open(data_file) as experimental:
         for data in experimental:
-            if data == '' or data == '\n' data.split() == []:
+            if data == '' or data == '\n' or data.split() == []:
                 continue
             counter+=1
             data_checker=re.findall('\d+\.\d+',data)
